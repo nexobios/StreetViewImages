87371db0b7cf0d987660a7fd8eccf78d05e103a2
F. Aguirre <fjaguirre@ymail.com>
2016-09-20 11:23:40 -0500

    Optional parameter Field of View (FOV) added
    This parameter allows to specify a range of vision from 0° to 120° by default it used to be 90°

--- Gmaps_StreetView.py
+++ Gmaps_StreetView.py
@@ -9,14 +9,27 @@ from PIL import Image
 
 def generate_request_to_gmaps(gps_location, image_angle, image_size, user_key):
     # Get sub-strings
+    image_parameters_str = []
     base_url = 'https://maps.googleapis.com/maps/api/streetview?'
     size_str = 'size=' + str(image_size[0]) + 'x' + str(image_size[1])
-    location_str = 'location=' + str(gps_location[0]) + ',' + str(gps_location[1])
-    heading_str = 'heading=' + str(image_angle[0])
-    pitch_str = 'pitch=' + str(image_angle[1])
-    key_str = 'key=' + user_key
+    image_parameters_str.append(size_str)
+    location_str = '&location=' + str(gps_location[0]) + ',' + str(gps_location[1])
+    image_parameters_str.append(location_str)
+    if image_angle[0] is not None:
+        heading_str = '&heading=' + str(image_angle[0])
+        image_parameters_str.append(heading_str)
+    if image_angle[1] is not None:
+        fov_str = '&fov=' + str(image_angle[1])
+        image_parameters_str.append(fov_str)
+    if image_angle[2] is not None:
+        pitch_str = '&pitch=' + str(image_angle[2])
+        image_parameters_str.append(pitch_str)
+    key_str = '&key=' + user_key
+    image_parameters_str.append(key_str)
     # Create request string
-    request_url = base_url + size_str + '&' + location_str + '&' + heading_str + '&' + pitch_str + '&' + key_str
+    request_url = base_url
+    for parameter in image_parameters_str:
+        request_url += parameter
     return request_url
 
 
@@ -25,11 +38,11 @@ def get_images_from_gmaps(key):
     user_key = str(key.googleKey)
     # TODO (FrAg): The following information must be automatically obtained from the traced path
     # Start Cfg
-    gps_location = [20.6206734, -103.4310546]
+    gps_location = [20.6162962, -103.2518853]
     # Size = [Width, Height]
     image_size = [640, 480]
-    # Angle = [Heading, Pitch]
-    image_angle = [174, 0]
+    # Angle = [Heading, FOV, Pitch]
+    image_angle = [None, 40, 0]
     # End of Cfg
     # Get the Image from GMaps
     url_API = generate_request_to_gmaps(gps_location, image_angle, image_size, user_key)
